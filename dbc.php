bf49b1c812c2eca5862a5f2284e490f3dbad7939
roexian <bryan.wangler@gmail.com>
2014-04-19 17:20:35 -0500

    Deleted unnecessary file
    Using a different file structure.

--- dbc.php
+++
@@ -1,6 +0,0 @@
-<?php
-$db = new PDO('mysql:host=localhost;port=8889;dbname=dingus;charset=utf8', 'writey', 'Iw2wad4m');
-$stmt = $db->prepare("INSERT INTO blog1(postTitle,postStory) VALUES(:postTitle,:postStory)");
-$stmt->execute(array(':postTitle' => $postTitle, ':postStory' => $postStory));
-?>
-	
